03d2f5f1653a1012b7c06917bad1e83219ae71e2
Benjamin Walter <waltermaussbenjamin@gmail.com>
2022-08-26 17:32:04 -0300

    testing

--- sw.js
+++ sw.js
@@ -1,63 +1,90 @@
-const CACHE = "pwabuilder-offline-page";
+// sw.js
 
-const offlineFallbackPage = "/offline.html";
+const staticCacheName = "cache_1"
+const filesToCache = [
+    "/LICENSE",
+    // "/app.webmanifest",
+    "/index.html",
+    "/sw.js",
+    "/imgs",
+    "/imgs/arrow_down.png",
+    "/imgs/handshake.png",
+    "/imgs/grafico.png",
+    "/imgs/carrinho.png",
+    "/imgs/arrow_up.png",
+    "/imgs/no-image2.png",
+    "/imgs/no-image.png",
+    "/offline.html",
+    "/README.md",
+    "/icon",
+    "/icon/512x512.png",
+    "/pages",
+    "/pages/vendas",
+    "/pages/vendas/index.html",
+    "/pages/historico",
+    "/pages/historico/index.html",
+    "/pages/produtos",
+    "/pages/produtos/index.html",
+    "/pages/produtos/novo",
+    "/pages/produtos/novo/index.html",
+    "/pages/produtos/editar",
+    "/pages/produtos/editar/index.html",
+    "/css",
+    "/css/novo.css",
+    "/css/index.css",
+    "/css/produtos.css",
+    "/css/vender.css",
+    "/css/mandatory.css",
+    "/css/historico.css",
+    "/favicon.ico",
+    "/.gitattributes",
+    "/js",
+    "/js/libraries",
+    "/js/libraries/jquery.min.js",
+    "/js/libraries/jquery.min.map",
+    "/js/mandatory.js",
+    "/fonts",
+    "/fonts/BebasNeue-Regular.ttf"
+]
 
-// Install stage sets up the offline page in the cache and opens a new cache
-self.addEventListener("install", function (event) {
-  console.log("[PWA Builder] Install Event processing");
+this.addEventListener("fetch", event => {
+    console.log("fetch")
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => {
+          return response || fetch(event.request);
+        })
+        .catch(() => {
+          return caches.match('/offline.html');
+        })
+    )
+  });
 
-  event.waitUntil(
-     caches.open(CACHE).then(function (cache) {
-      console.log("[PWA Builder] Cached offline page during install");
-
-      if (offlineFallbackPage === "offline.html") {
-        return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
-      }
-
-      return cache.add(offlineFallbackPage);
-    })
-  );
+// Cache on install
+this.addEventListener("install", event => {
+    console.log("installing/caching");
+    this.skipWaiting();
+  
+    event.waitUntil(
+      caches.open(staticCacheName)
+        .then(cache => {
+          return cache.addAll(filesToCache);
+      })
+    )
 });
 
-//https://developers.google.com/web/updates/2017/02/navigation-preload
-//Acelerar al trabajador de servicio con precargas de navegación
-self.addEventListener('fetch', event => {
-  event.respondWith(async function() {
-    // Respond from the cache if we can
-    const cachedResponse = await caches.match(event.request);
-    if (cachedResponse) return cachedResponse;
-
-    // Else, use the preloaded response, if it's there
-    const response = await event.preloadResponse;
-    if (response) return response;
-
-    // Else try the network.
-    return fetch(event.request);
-  }());
-});
-
-function fromCache(request) {
-  // Check to see if you have it in the cache
-  // Return response
-  // If not in the cache, then return the offline page
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      if (!matching || matching.status === 404) {
-        // The following validates that the request was for a navigation to a new document
-        if (request.destination !== "document" || request.mode !== "navigate") {
-          return Promise.reject("no-match");
-        }
-
-        return cache.match(offlineFallbackPage);
-      }
-
-      return matching;
-    });
+// Clear cache on activate
+this.addEventListener('activate', event => {
+    console.log("activating/removing old cache");
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames
+            .filter(cacheName => (cacheName.startsWith('cache_')))
+            .filter(cacheName => (cacheName !== staticCacheName))
+            .map(cacheName => caches.delete(cacheName))
+        );
+      })
+    );
   });
-}
-
-function updateCache(request, response) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.put(request, response);
-  });
-}
+  
